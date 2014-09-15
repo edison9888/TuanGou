f6d550db91a8f6069b1c9e63b49b270e052547a6
叶根长 <yegenchang@yegenchangdeMac-mini.local>
2014-09-15 21:48:18 +0800

    Initial Commit

---
+++ MeiTuan/MTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTAppDelegate.h
+//  MeiTuan
+//
+//  Created by 叶根长 on 14-9-15.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeiTuan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeiTuan
+//
+//  Created by 叶根长 on 14-9-15.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTAppDelegate class]));
+    }
+}
